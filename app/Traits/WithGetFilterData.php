9deff883748d98eedf47079eb5ce6ce666ffbc82
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2024-11-12 10:34:52 +0700

    Update WithGetFilterData.php

--- app/Traits/WithGetFilterData.php
+++ app/Traits/WithGetFilterData.php
@@ -7,21 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 trait WithGetFilterData {
 
-    public function getDataWithFilter(Model|Builder $model, array $searchBy = [
-        [
-            'name' => '',
-            'field' => '',
-            'no_search' => true,
-        ]
-    ], string $orderBy = 'id', string $order = 'asc', int $paginate = 10, string $s = '') {
+    public function getDataWithFilter(Model|Builder $model, array $searchBy = [], string $orderBy = 'id', string $order = 'asc', int $paginate = 10, string $s = '') {
 
         $model = $model->where(function ($query) use ($s, $searchBy) {
-            foreach ($searchBy as $key => $value) {
-                if(!isset($value['no_search'])) {
-                    $field = $value['field'];
-
-                    $query->orWhere($field, 'like', "%$s%");
-                }
+            foreach ($searchBy as $value) {
+                $query->orWhere($value, 'like', "%$s%");
             }
         });
 
